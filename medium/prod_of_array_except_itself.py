fa46760032c29cf6f9cbed79aa01e340cb3eb3ff
asinha4 <ankitasinha0811@gmail.com>
2021-06-06 21:33:16 +0530

    medium

---
+++ medium/prod_of_array_except_itself.py
@@ -0,0 +1,19 @@
+import numpy as np
+def productExceptSelf(nums):
+    prod_left = np.ones(len(nums))
+    prod_right = np.ones(len(nums))
+    prod = np.zeros(len(nums))
+    for i in range(1,len(nums)):
+        prod_left[i] = int(prod_left[i-1]*nums[i-1])
+    for i in range(len(nums)-2, -1, -1):
+        prod_right[i] = int(prod_right[i+1]*nums[i+1])
+    for i in range(len(nums)):
+        prod[i] = int(prod_right[i]* prod_left[i])
+
+    return prod
+
+
+
+
+if __name__ == '__main__':
+    print(productExceptSelf([1,2,3,4]))
--- medium/trapping_rain_water.py
+++ medium/trapping_rain_water.py
@@ -10,11 +10,30 @@ def trap(height):
 
     return units
 
+def trap_dp(height):
+    if(len(height) == 0):
+        return 0
+
+    left_max = [height[0]]
+    for i in range(1, len(height)):
+        left_max.append(max(height[:i]))
+
+    right_max = []
+    for i in range(len(height)-1):
+        right_max.append(max(height[i+1:]))
+    right_max.append(height[-1])
+
+    units = 0
+    for i in range(len(height)):
+        sum = min(left_max[i], right_max[i]) - height[i]
+        units += sum if sum > 0 else 0
+    return units
+
 
 
 if __name__ == '__main__':
-    print(trap([4,2,0,3,2,5]))
+    print(trap_dp([4,2,0,3,2,5]))
     # print(trap([3,2,1,2,1]))
-    print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
-    print(trap([4,2,3]))
+    print(trap_dp([0,1,0,2,1,0,1,3,2,1,2,1]))
+    print(trap_dp([4,2,3]))
 

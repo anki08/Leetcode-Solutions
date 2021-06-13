1a98dd7d9ae09e439b0b44ae48fcb965bb6ecba5
asinha4 <ankitasinha0811@gmail.com>
2021-06-13 15:57:53 +0530

    Update spiral-matrix.py

--- medium/spiral-matrix.py
+++ medium/spiral-matrix.py
@@ -1,36 +1,24 @@
 import numpy as np
 
 def spiralOrder(matrix):
-    num_rows,num_cols = matrix.shape
-    start_r , start_c = 0,0
+    num_rows, num_cols = matrix.shape
     res = []
-    r,c = 0,0
-    while start_r<num_rows and start_c < num_cols:
-        while r < num_rows and c < num_cols:
-                res.append(matrix[r][c])
-                if(r == start_r and c == start_c):
-                    c += 1
-                elif(r == start_r and c == num_cols-1):
-                    r += 1
-                elif(r==num_rows and c==num_cols):
-                    c -=1
-                elif(r == num_rows and c == start_c):
-                    r -= 1
-                else:
-                    c+=1
-            # r += 1
-
-        start_c += 1
-        start_r += 1
-        num_cols -= 1
-        num_rows -= 1
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0]
+    r, c , di = 0, 0, 0
+    seen = np.zeros((num_rows, num_cols), dtype=int)
+    for i in range(num_cols * num_rows):
+        res.append(matrix[r][c])
+        seen[r][c] = 1
+        nr, nc = r + dr[di], c + dc[di]
+        if 0<=nc<num_cols and 0<=nr<num_rows and seen[nr][nc] == 0:
+            r,c = nr,nc
+        else:
+            di = (di + 1)%4
+            r,c = r + dr[di], c + dc[di]
     return res
 
 
-
-
-
 if __name__ == '__main__':
-
     matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     print(spiralOrder(matrix))

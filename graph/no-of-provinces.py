0bbd3e013952057d1a5d0b272ec99b1d64abc83b
asinha4 <ankitasinha0811@gmail.com>
2021-07-04 17:04:29 +0530

    Union find graphs

--- graph/no-of-provinces.py
+++ graph/no-of-provinces.py
@@ -1,6 +1,27 @@
 import numpy as np
 def findCircleNum(isConnected) -> int:
-    parent =
+    n = len(isConnected)
+    parent = {x:x for x in range(n)}
+    print(parent)
+
+    def find(node):
+        if (parent[node] == node):
+            return node
+        root = find(parent[node])
+        parent[node] = root
+        return root
+
+    def union(node1, node2):
+        if find(node1) == find(node2):
+            return
+        parent[find(node1)] = find(node2)
+
+    for i in range(n):
+        for j in range(i,n):
+            if(i!=j and isConnected[i][j] == 1):
+                union(i,j)
+    print(parent)
+    return sum(1 for k,v in parent.items() if k==v)
 
 
 
